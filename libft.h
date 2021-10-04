babe68cd10444becfbda34bf26cd7b018d6e179b
jreverdy <jeremy.reverdy.zjr@gmail.com>
2021-10-04 18:53:46 +0200

    fixed my dumbed mistakes

--- libft.h
+++ libft.h
@@ -7,6 +7,6 @@
 int ft_atoi(const char *str);
 size_t ft_strlen(const char *s);
 size_t ft_strlcpy(char *dst, const char *src, size_t size);
-char *ft_strdup(const char *s)
+char *ft_strdup(const char *s);
 
 #endif //LIBFT_LIBFT_H
