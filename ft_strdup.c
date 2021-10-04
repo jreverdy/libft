03f2e3ea9b47ca7c52e54a4f1ce7202c406c1b6c
root <root@DESKTOP-VD03MRH.localdomain>
2021-10-04 18:51:21 +0200

    fixed errors on ft_strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -2,15 +2,13 @@
 
 char *ft_strdup(const char *s)
 {
-    size_t  i;
     size_t  len;
     char *str;
 
-    i = 0;
     len = ft_strlen(s) + 1;
     if (s == NULL)
         return (NULL);
-    str = malloc(sizeof(char) * (len);
+    str = malloc(sizeof(char) * len);
     if (str == NULL)
         return (NULL);
     ft_strlcpy(str, s, len);
