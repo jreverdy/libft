36ede9c499b51e093ad94829be0f5d6f7ea67b7c
jreverdy <jeremy.reverdy.zjr@gmail.com>
2021-10-09 15:30:39 +0200

    commit ft_is* functions + ft_strncmp.c

--- libft.h
+++ libft.h
@@ -8,5 +8,13 @@ int ft_atoi(const char *str);
 size_t ft_strlen(const char *s);
 size_t ft_strlcpy(char *dst, const char *src, size_t size);
 char *ft_strdup(const char *s);
+int ft_strncmp(const char *s1, const char *s2, size_t n);
+int ft_isalpha(int c);
+int ft_isdigit(int c);
+int ft_isalnum(int c);
+int ft_isascii(int c);
+int ft_isprint(int c);
+int ft_toupper(int c);
+int ft_tolower(int c);
 
 #endif //LIBFT_LIBFT_H
--- main.c
+++ main.c
@@ -3,13 +3,22 @@
 #include "libft.h"
 #include <limits.h>
 #include <string.h>
+#include <ctype.h>
 
-int main() {
-
-    char *str;
-
-    str = malloc(sizeof(char) * 8);
-    printf(" ft_strlcpy = %ld\n", ft_strlcpy(str, "bonjour", 8));
-    printf("%s\n", str);
-    return 0;
+int main()
+{
+    int i = 65;
+    int j = 65;
+    printf("%d\n", ft_tolower(i));
+    printf("%d\n", tolower(j));
 }
+
+/*int main()
+{
+    char *str1 = "bon\0jour";
+    char *str2 = "bon\0dour";
+    size_t i = 8;
+    printf(" ft_strncmp = %d\n", ft_strncmp(str1, str2, i));
+    printf(" strncmp = %d\n", strncmp(str1, str2, i));
+    return 0;
+}*/
